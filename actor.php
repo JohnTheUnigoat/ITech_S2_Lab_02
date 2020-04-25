ef1d1b9dc3e73e6a59b68647b60c012f68de4c25
Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>
2020-04-26 00:14:30 +0300

    Simplified actors query
    Signed-off-by: Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>

--- actor.php
+++ actor.php
@@ -5,7 +5,7 @@ require 'db.php';
 $actor = $_GET['actor'];
 
 $res = $collection->find(
-    ['actors' => ['$eq' => $actor]],
+    ['actors' => $actor],
     ['projection' =>
         ['_id' => 0, 'name' => 1, 'release_year' => 1, 'director' => 1, 'media' => 1]
     ]
